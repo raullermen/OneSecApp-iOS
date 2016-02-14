22998750a29f272d91f695793f0c45f5f9b42763
Raul Lermen <raullermen@live.com>
2016-02-14 17:37:30 -0200

    Initial Commit

---
+++ OneSecApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  OneSecApp
+//
+//  Created by Raul Lermen on 2/14/16.
+//  Copyright © 2016 Raul Lermen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
